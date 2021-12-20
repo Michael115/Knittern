add3cd5f834fbb12f58205047d12e9b217a3bed4
Michael <michael@blocksui.com>
2021-12-20 16:00:05 +0000

    Convert everything to ts files

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import "tailwindcss/tailwind.css";
+import "../public/fonts/font.css";
+import { AppProps } from 'next/app';
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
+
+export default MyApp;
